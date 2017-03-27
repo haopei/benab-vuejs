9a32df99b92575b3e0aa6a60a75242f55cac7a8b
Haopei Yang <haopeiyang@gmail.com>
2017-03-27 19:52:37 -0400

    Create GetInvolved.vue

---
+++ src/components/GetInvolved.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <section class="section is-medium">
+      <div class="container">
+        <div class="heading"><h1 class="title">Get Involved</h1></div>
+        <h2 class="subtitle">Be part of the magic</h2>
+        <div class="columns">
+            <div class="column"><h3 class="subtitle is-3">Become a Volunteer</h3></div>
+            <div class="column"><h3 class="subtitle is-3">Become a Sponsor</h3></div>
+            <div class="column"><h3 class="subtitle is-3">Join Our Team</h3></div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
