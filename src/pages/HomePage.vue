34ab6c5ec2d0e7843b7f50ef8200e0f9c95dbd4e
Haopei Yang <haopeiyang@gmail.com>
2017-03-27 19:06:52 -0400

    listed requirements per page (on the page)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="page page--home">
     <h3>Home</h3>
+
+    <div class="requirements">
+        <ul>
+            <li>Hero</li>
+            <li>Cog2016</li>
+            <li>Other Projects</li>
+            <li>List of Sponsors</li>
+            <li>Volunteers</li>
+        </ul>
+    </div>
+
   </div>
 </template>
 
--- src/pages/SponsorPage.vue
+++ src/pages/SponsorPage.vue
@@ -1,6 +1,14 @@
 <template lang="html">
     <div class="page page--sponsor">
         <h3>Sponsor</h3>
+        <div class="requirements">
+            <ul>
+                <li>Hero</li>
+                <li>Encourage companies to sponsor with good reason</li>
+                <li>List of existing sponsors</li>
+                <li>Contact component</li>
+            </ul>
+        </div>
     </div>
 </template>
 
