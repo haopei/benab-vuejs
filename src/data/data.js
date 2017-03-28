73e793e79d9f765753056897fa774eb259f1ad64
Haopei Yang <haopeiyang@gmail.com>
2017-03-27 22:53:28 -0400

    Created raw data.js file with 'allProjects' array exported

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const allProjects = [
+    {
+        name: "Cognitive Initiative",
+        shortDescription: "A one-month program geared towards leveling up Mathematics and English Language skills in under-performing students across five schools.",
+        yearStarted: '2013',
+        image: 'benab-code/src/assets/logo.png'
+    }
+]
