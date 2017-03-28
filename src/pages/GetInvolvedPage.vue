4c66ed7bd3da98b5ce722ea03865e0ba0900dba3
Haopei Yang <haopeiyang@gmail.com>
2017-03-28 13:53:04 -0400

    Structured GetInvolved.vue

--- src/pages/GetInvolvedPage.vue
+++ src/pages/GetInvolvedPage.vue
@@ -1,6 +1,52 @@
 <template lang="html">
     <div class="page page--get-involved">
-        <h3>Get Involved</h3>
+        <section class="hero is-medium is-primary is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Get Involved
+              </h1>
+              <h2 class="subtitle">
+                  Some inspirational sentence, or something.
+              </h2>
+            </div>
+          </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="columns has-text-centered">
+                    <div class="column">
+                        <h4 class="subtitle is-4">Volunteer</h4>
+                    </div>
+                    <div class="column">
+                        <h4 class="subtitle is-4">Donate</h4>
+                    </div>
+                    <div class="column">
+                        <h4 class="subtitle is-4">Join Our Team</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h2></h2>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h2></h2>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <h2></h2>
+            </div>
+        </section>
+
         <div class="requirements">
             <ul>
                 <li>Hero</li>
