65dbc618a1605df9023d1ef895b1c04c7a13b8d4
Haopei Yang <haopeiyang@gmail.com>
2017-03-27 22:53:55 -0400

    Create AllProjectsList.vue component

---
+++ src/components/AllProjectsList.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <section class="section">
+      <div class="container">
+        <div v-for="project in allProjects" class="project-item">
+          <pre>{{project}}</pre>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import { allProjects } from './../data/data.js'
+export default {
+  data() {
+    return {
+      allProjects: []
+    }
+  },
+  mounted() {
+    this.allProjects = allProjects;
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
