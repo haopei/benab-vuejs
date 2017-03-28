8fa47a3e475cd433a35aed11f75976226dada11d
Haopei Yang <haopeiyang@gmail.com>
2017-03-28 19:53:40 -0400

    Minor: change css class name partial--cog-hero to background--cog-hero

--- src/components/CogPartial/CogPartial.vue
+++ src/components/CogPartial/CogPartial.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <section class="section partial partial--cog-hero has-text-centered is-large">
+    <section class="section partial background--cog-hero has-text-centered is-large">
         <div class="container">
             <img src="./cog-title-text.png" alt="">
             <h2 class="subtitle">Leveling up students who are preparing for their 3rd Form Exams</h2>
@@ -31,11 +31,10 @@ export default {
 </script>
 
 <style lang="scss">
-.partial--cog-hero {
+.background--cog-hero {
     background: #4C86A8; /* fallback for old browsers */
     background: -webkit-linear-gradient(141deg, #4C86A8 , #AF9164); /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(-135deg, #4C86A8 , #AF9164); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
-
 
 </style>
