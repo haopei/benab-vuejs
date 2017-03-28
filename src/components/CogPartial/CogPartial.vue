5843cf8744dee2a27828eff19d5852b88caa8c47
Haopei Yang <haopeiyang@gmail.com>
2017-03-28 19:15:37 -0400

    Implement 'showInvolvementOptions in CogPartial.vue

--- src/components/CogPartial/CogPartial.vue
+++ src/components/CogPartial/CogPartial.vue
@@ -3,13 +3,30 @@
         <div class="container">
             <img src="./cog-title-text.png" alt="">
             <h2 class="subtitle">Leveling up students who are preparing for their 3rd Form Exams</h2>
-            <router-link :to="{ name: 'GetInvolvedPage' }"><a class="button is-large">Get Involved</a></router-link>
+            <router-link :to="{ name: 'Cog2017Page' }"><a class="button is-primary is-large">Learn More</a></router-link> <br>
+            <div class="padded-space"></div>
+            <button v-if="!showInvolvementOptions" class="button is-link" @click="handleClick">Get Involved</button>
+            <div v-if="showInvolvementOptions" class="involvement-options">
+                <router-link :to="{ name: 'Cog2017Page' }"><a class="button">Volunteer Tutor</a></router-link>
+                <router-link :to="{ name: 'Cog2017Page' }"><a class="button">Donate</a></router-link>
+                <router-link :to="{ name: 'Cog2017Page' }"><a class="button">Join Team</a></router-link>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            showInvolvementOptions: false
+        }
+    },
+    methods: {
+        handleClick(e) {
+            this.showInvolvementOptions = true;
+        }
+    }
 }
 </script>
 
@@ -19,4 +36,6 @@ export default {
     background: -webkit-linear-gradient(141deg, #4C86A8 , #AF9164); /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(-135deg, #4C86A8 , #AF9164); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
+
+
 </style>
