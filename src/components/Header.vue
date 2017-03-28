06bd22404bf44a0efd9555db74fb36e6c002bbd8
Haopei Yang <haopeiyang@gmail.com>
2017-03-28 13:21:13 -0400

    /projects is handled by ProjectsPage instead of AllProjectsList

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,7 @@
         <div class="nav-right nav-menu">
             <router-link :to="{ name: 'HomePage' }" class="nav-item is-tab"><a @click="handleLinkClick">Home</a></router-link>
             <router-link :to="{ name: 'Cog2017Page' }" class="nav-item is-tab"><a @click="handleLinkClick">Cog2017</a></router-link>
-            <router-link :to="{ name: 'AllProjectsList' }" class="nav-item is-tab"><a @click="handleLinkClick">Projects</a></router-link>
+            <router-link :to="{ name: 'ProjectsPage' }" class="nav-item is-tab"><a @click="handleLinkClick">Projects</a></router-link>
             <router-link :to="{ name: 'AboutPage' }" class="nav-item is-tab"><a @click="handleLinkClick">About</a></router-link>
 
           <span class="nav-item">
