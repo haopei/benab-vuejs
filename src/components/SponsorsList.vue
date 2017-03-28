178a70f939b11b6d7a4319af89ed818ebaebefeb
Haopei Yang <haopeiyang@gmail.com>
2017-03-27 23:11:25 -0400

    Created SponsorsList.vue component

---
+++ src/components/SponsorsList.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <section class="section is-medium">
+      <div class="container">
+        <div class="heading"><h1 class="title">Sponsors</h1></div>
+        <h2 class="subtitle">Our gratitude to our sponsors who make it happen</h2>
+        <pre>{{ sponsors }}</pre>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['sponsors']
+}
+</script>
+
+<style lang="css">
+</style>
