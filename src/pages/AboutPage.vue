52f1b6b19ebcbdecfcfddfc14721ac62c3807199
Haopei Yang <haopeiyang@gmail.com>
2017-03-28 13:52:32 -0400

    Structured About.vue

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,14 +1,36 @@
 <template lang="html">
   <div class="page page--about">
-      <h3>About</h3>
-      <div class="requirements">
-          <ul>
-              <li>Hero</li>
-              <li>Story</li>
-              <li>History</li>
-              <li>Values</li>
-          </ul>
-      </div>
+      <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Our Story
+            </h1>
+            <h2 class="subtitle">
+              In short, we are just a bunch of energetic, young folks who can see better.
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container">
+          <h2 class="title">Our Story</h2>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container">
+          <h2 class="title">Our Team</h2>
+          [list of core team]
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container">
+          <h2 class="title">Our Mission</h2>
+        </div>
+      </section>
   </div>
 </template>
 
