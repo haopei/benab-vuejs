4911546dbb3975b37aa89336edb02a52c320923b
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 07:11:31 -0400

    Implement testimonials in SponsorsList.vue

--- src/components/SponsorsList.vue
+++ src/components/SponsorsList.vue
@@ -5,12 +5,28 @@
         <h3 class="subtitle">Our gratitude to our sponsors who make it happen</h3>
         <pre>{{ sponsors }}</pre>
       </div>
+
+      <testimonial v-for="t in testimonials" :testimonial="t" :key="t.name"></testimonial>
     </section>
 </template>
 
 <script>
+import Testimonial from './Testimonial.vue'
+import { featuredSponsorshipTestimonial } from './../data/testimonials.data.js'
+
 export default {
-    props: ['sponsors']
+    components: {
+        Testimonial
+    },
+    props: ['sponsors'],
+    data() {
+        return {
+            testimonials: []
+        }
+    },
+    mounted: function() {
+        this.testimonials = featuredSponsorshipTestimonial;
+    }
 }
 </script>
 
