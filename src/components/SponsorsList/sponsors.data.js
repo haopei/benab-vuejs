8414623b7c20403774984131c6cc9ee0f0039288
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 14:49:07 -0400

    Combine sponsors into 'allCogSponsors' const

--- src/components/SponsorsList/sponsors.data.js
+++ src/components/SponsorsList/sponsors.data.js
@@ -9,26 +9,80 @@
 //      }
 // ]
 
-export const cogSponsors2016 = [
-    {
-         name: 'Church\'s Chicken',
-         contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-         image: require('./static/churchs.png')
-     }
-]
+export const allCogSponsors = {
+    year2017: [
+        {
+             name: 'You',
+             contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+             image: require('./static/churchs.png')
+         }
+    ],
+    year2016: [
+        {
+             name: 'Western Union',
+             contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+             image: require('./static/churchs.png')
+         }
+    ],
+    year2015: [
+        {
+             name: 'Church\'s Chicken',
+             contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+             image: require('./static/churchs.png')
+         }
+    ],
+    year2014: [
+        {
+             name: 'Some Company',
+             contribution: 'Donated x amount of money and more',
+             image: require('./static/western-union.png')
+         }
+    ],
+    year2013: [
+        {
+             name: 'Some Company',
+             contribution: 'Donated x amount of money and more',
+             image: require('./static/western-union.png')
+         }
+    ]
+}
 
-export const cogSponsors2015 = [
-    {
-         name: 'Some Company',
-         contribution: 'Donated x amount of money and more',
-         image: require('./static/western-union.png')
-     }
-]
-
-export const cogSponsors2014 = [
-    {
-         name: 'Some Organisation',
-         contribution: 'Donated x amount of money and more',
-         image: 'src'
-     }
-]
+// export const cogSponsors2016 = [
+//     {
+//          name: 'Church\'s Chicken',
+//          contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+//          image: require('./static/churchs.png')
+//      }
+// ]
+//
+// export const cogSponsors2016 = [
+//     {
+//          name: 'Church\'s Chicken',
+//          contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+//          image: require('./static/churchs.png')
+//      }
+// ]
+//
+// export const cogSponsors2015 = [
+//     {
+//          name: 'Some Company',
+//          contribution: 'Donated x amount of money and more',
+//          image: require('./static/western-union.png')
+//      }
+// ]
+//
+// export const cogSponsors2014 = [
+//     {
+//          name: 'Some Organisation',
+//          contribution: 'Donated x amount of money and more',
+//          image: 'src'
+//      }
+// ]
+//
+// export const cogSponsors2013 = [
+//     {
+//          name: 'Some Organisation 2013',
+//          contribution: 'Donated x amount of money and more',
+//          image: 'src'
+//      }
+// ]
