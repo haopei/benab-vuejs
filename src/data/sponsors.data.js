ed30e68d5cd827087d9484687bbeb1aa7103f68d
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 13:43:50 -0400

    Remodelled sponsors data to include sponsors per year

--- src/data/sponsors.data.js
+++ src/data/sponsors.data.js
@@ -1,4 +1,16 @@
 export const allSponsors = [
-    { name: 'Grace Kennedy Remittance Services', rank: 'gold' },
-    { name: 'Version75', rank: 'Bronze' }
+    { name: 'Grace Kennedy Remittance Services', contribution: 'Provided branded uniform to volunteers during the exercise.' },
+    { name: 'Version75', contribution: '' }
+]
+
+export const cogSponsors2016 = [
+    { name: 'Church\'s Chicken', contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.' }
+]
+
+export const cogSponsors2015 = [
+    { name: 'Some Company', contribution: 'Donated x amount of money and more' }
+]
+
+export const cogSponsors2014 = [
+    { name: 'Some Organisation', contribution: 'Donated x amount of money and more' }
 ]
