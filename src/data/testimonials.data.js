d4af08eca0d1b80d72220561d2a9651c8b00fdf0
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 07:10:26 -0400

    Create testimonials.data.js to contain all testimonials

---
+++ src/data/testimonials.data.js
@@ -0,0 +1,15 @@
+export const cog2017Testimonials = [
+    {
+        name: 'Maria',
+        quote: 'One of the most rewarding experience of my life!',
+        role: 'Volunteer Tutor, Cognitive Initiative 2016'
+    }
+]
+
+export const featuredSponsorshipTestimonial = [
+    {
+        name: 'Grace Kennedy Remittance Services',
+        quote: 'Among the most energetic group of young people we have worked with.',
+        role: 'Gold Sponsor, Cognitive Initiative 2016'
+    }
+]
