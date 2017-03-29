669aaa97aa2398c6bb5e11bebdc8e65b566807ac
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 19:56:58 -0400

    Minor: cleanup

--- src/components/GetInvolved.vue
+++ src/components/GetInvolved.vue
@@ -3,26 +3,27 @@
       <div class="container">
         <div class="heading"><h2 class="title is-2">Get Involved</h2></div>
         <h6 class="subtitle is-h6">Be part of the magic</h6>
+        [use tabs for this]
         <hr>
         <div class="columns">
             <div class="column">
                 <h4 class="subtitle is-4">Become a Volunteer</h4>
                 <img :src="require('./../assets/collage.jpg')" alt="">
-                <p class="small">It will be the one of the most rewarding experiences in your life</p>
+                <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
                 <router-link :to="{name: 'VolunteerPage'}" class="button is-primary is-outlined is-inverted">Volunteer</router-link>
             </div>
             <div class="column">
                 <h4 class="subtitle is-4">Become a Sponsor</h4>
                 <img :src="require('./../assets/collage.jpg')" alt="">
-                <p class="small">It will be the one of the most rewarding experiences in your life</p>
+                <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
                 <router-link :to="{name: 'SponsorPage'}" class="button is-primary is-outlined is-inverted">Sponsor Our Initiatives</router-link>
             </div>
             <div class="column">
                 <h4 class="subtitle is-4">Join Our Team</h4>
                 <img :src="require('./../assets/collage.jpg')" alt="">
-                <p class="small">It will be the one of the most rewarding experiences in your life</p>
+                <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
                 <router-link :to="{name: 'MembershipPage'}" class="button is-primary is-outlined is-inverted">Join Our Team</router-link>
             </div>
@@ -38,7 +39,7 @@ export default {
 
 <style lang="scss" scoped>
 .partial--get-involved {
-    background: #477890;
+    background: #282c37;
     color: #fff;
 }
 
--- src/pages/Cog2017Page/Cog2017Page.vue
+++ src/pages/Cog2017Page/Cog2017Page.vue
@@ -7,31 +7,17 @@
               <h1 class="title is-spaced">
                   <img src="./cog-title-text.png" alt="">
               </h1>
-              <h2 class="subtitle">
-                  <h2>Make this carnival and education</h2>
-                 Leveling up Mathematics and English Language in underperforming students <br>
-                 <ul>
-                     <li>photos of year 1, 2, 3, 4</li>
-                     <li>estimation of improvements</li>
-                     <li>honorable mentions (special thank yous)</li>
-                     <li>Donors per year</li>
-                 </ul>
-                 <br>
-                 <ul>
-                    <h2>Thank you page</h2>
-                     <li>Jennifer Cumberbach: big supporter </li>
-                     <li>carryl: loyal sponsor</li>
-                     <li>Volunteers</li>
-                     <li>Churches</li>
-                 </ul>
+              <h2 class="subtitle page-cog-2017__quirks--left-padding">
+                  <small>
+                      Every year, since 2013, we recruit 30 top-performing volunteers <br> to teach Mathematics and English to 100 students <br> from under-performing schools.
+                  </small>
               </h2>
             </div>
           </div>
         </section>
 
-        <section class="section">
+        <section class="section page-cog-2017__overview">
             <div class="container">
-
                 <div class="content">
                     <div class="columns">
                         <div class="column">
@@ -42,28 +28,61 @@
                             <iframe width="560" height="315" src="https://www.youtube.com/embed/Bh6PFNfveac" frameborder="0" allowfullscreen></iframe>
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
-
-        <strong>Elements of focus: queens college, partitioned students based on level, one-on-one focus</strong>
 
         <section class="section">
             <div class="container">
                 <div class="columns">
                     <div class="column">
                         <h4 class="title is-4">Queen's College</h4>
-                        <p>Say something about this</p>
+                        <p>The initiative takes place at Queen's College where students get to experience both its environment and culture.</p>
                     </div>
                     <div class="column">
                         <h4 class="title is-4">Tailored Curriculum</h4>
-                        <p>Also say something about this</p>
+                        <p>Students are first assessed and then grouped by competencies so that tailored lessons are available.</p>
                     </div>
                     <div class="column">
                         <h4 class="title is-4">Top Performing Volunteers</h4>
-                        <p>And finally, say something like this</p>
+                        <p>Volunteer tutors must have a Grade I in both Mathematics and English Language, in addition to other favourable qualities.</p>
                     </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section page-cog-2017__students">
+            <div class="container">
+                <h2 class="title">Needs of the initiative</h2>
+                <h4 class="subtitle is-h4">[This is a checklist of things we need]</h4>
+                <div class="columns">
+                    <div class="column">
+                        <ul>
+                            <li>360 meals for ~120 students for 30 days.</li>
+                            <li>Water and drinks for 30 days</li>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                        </ul>
+                    </div>
+
+                    <div class="column">
+                        <ul>
+                            <li>30 Volunteer Tutors</li>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                        </ul>
+                    </div>
+
+                    <div class="column">
+                        <ul>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                            <li>Some other thing</li>
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </section>
@@ -76,6 +95,7 @@
         </section>
 
         <sponsors-list></sponsors-list>
+        <get-involved></get-involved>
 
     </div>
 </template>
@@ -83,18 +103,40 @@
 <script>
 
 import SponsorsList from './../../components/SponsorsList/SponsorsList.vue'
+import GetInvolved from './../../components/GetInvolved.vue'
 
 export default {
     components: {
-        SponsorsList
+        SponsorsList,
+        GetInvolved
     }
 }
 </script>
 
 <style lang="scss">
-.background--cog-hero {
-    background: #4C86A8; /* fallback for old browsers */
-    background: -webkit-linear-gradient(141deg, #4C86A8 , #AF9164); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(-135deg, #4C86A8 , #AF9164); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+.page-cog-2017 {
+    &__hero {
+        background: #4C86A8; /* fallback for old browsers */
+        background: -webkit-linear-gradient(141deg, #4C86A8 , #AF9164); /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(-135deg, #4C86A8 , #AF9164); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    }
+
+    &__quirks {
+        &--left-padding {
+            padding-left: 1rem;
+        }
+    }
+
+    &__overview {
+        background: #E0EAFC; /* fallback for old browsers */
+        background: -webkit-linear-gradient(to left, #E0EAFC , #CFDEF3); /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to bottom, #E0EAFC , #CFDEF3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+
+    &__students {
+
+    }
 }
 </style>
