f5c4a31e55e88fd99c2c67ecde91c76ba36341f4
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 14:49:59 -0400

    Add <sponsors-list> to HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,14 +3,14 @@
 
     <cog-partial></cog-partial>
 
-    <section class="section">
+    <!-- <section class="section">
         <div class="container">
             <h2>Our Impact</h2>
         </div>
-    </section>
+    </section> -->
 
     <get-involved></get-involved>
-    <sponsors-list :sponsors="sponsors"></sponsors-list>
+    <sponsors-list></sponsors-list>
 
   </div>
 </template>
@@ -19,31 +19,20 @@
 
 // Component Imports
 import GetInvolved from './../components/GetInvolved.vue'
-import SponsorsList from './../components/SponsorsList.vue'
+import SponsorsList from './../components/SponsorsList/SponsorsList.vue'
 
 // Partial Imports
 import CogPartial from './../components/CogPartial/CogPartial.vue'
-
-// Data Imports
-import { allSponsors } from './../data/sponsors.data.js'
 
 export default {
   components: {
     GetInvolved,
     SponsorsList,
     CogPartial
-  },
-  data() {
-    return {
-      sponsors: []
-    }
-  },
-  mounted: function() {
-    this.sponsors = allSponsors
   }
 }
 </script>
 
 <style lang="scss">
-  
+
 </style>
