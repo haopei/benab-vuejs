b5a42190a633604cfb369f4f2cfcb5d507167f0e
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 07:10:03 -0400

    Create Testimonial.vue

---
+++ src/components/Testimonial.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <article class="media testimonial">
+    <figure class="media-left">
+      <p class="image is-64x64">
+        <img src="http://bulma.io/images/placeholders/128x128.png">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+            <em>{{ testimonial.quote }}</em>""
+          <br>
+          <strong>{{ testimonial.name }}</strong>
+          <br>
+          <small>{{ testimonial.role }}</small>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['testimonial']
+}
+</script>
+
+<style lang="css">
+</style>
