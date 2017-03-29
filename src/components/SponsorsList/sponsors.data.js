0af4c173a90f3b4fee752e1b2eef0130f02cb632
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 15:20:40 -0400

    Resized sponsor logo images to 128x128

--- src/components/SponsorsList/sponsors.data.js
+++ src/components/SponsorsList/sponsors.data.js
@@ -14,75 +14,55 @@ export const allCogSponsors = {
         {
              name: 'You',
              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-             image: require('./static/churchs.png')
+             image: require('./static/churchs-128x128.png')
          }
     ],
     year2016: [
         {
              name: 'Western Union',
              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-             image: require('./static/churchs.png')
-         }
+             image: require('./static/churchs-128x128.png')
+         },
+         {
+              name: 'Western Union',
+              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+              image: require('./static/western-union-128x128.png')
+          }
     ],
     year2015: [
         {
-             name: 'Church\'s Chicken',
+             name: 'Western Union',
              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-             image: require('./static/churchs.png')
-         }
+             image: require('./static/churchs-128x128.png')
+         },
+         {
+              name: 'Western Union',
+              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+              image: require('./static/western-union-128x128.png')
+          }
     ],
     year2014: [
         {
-             name: 'Some Company',
-             contribution: 'Donated x amount of money and more',
-             image: require('./static/western-union.png')
-         }
+             name: 'Western Union',
+             contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+             image: require('./static/churchs-128x128.png')
+         },
+         {
+              name: 'Western Union',
+              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+              image: require('./static/western-union-128x128.png')
+          }
     ],
     year2013: [
         {
-             name: 'Some Company',
-             contribution: 'Donated x amount of money and more',
-             image: require('./static/western-union.png')
-         }
+             name: 'Western Union',
+             contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+             image: require('./static/churchs-128x128.png')
+         },
+         {
+              name: 'Western Union',
+              contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+              image: require('./static/western-union-128x128.png')
+          }
     ]
 }
-
-// export const cogSponsors2016 = [
-//     {
-//          name: 'Church\'s Chicken',
-//          contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-//          image: require('./static/churchs.png')
-//      }
-// ]
-//
-// export const cogSponsors2016 = [
-//     {
-//          name: 'Church\'s Chicken',
-//          contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
-//          image: require('./static/churchs.png')
-//      }
-// ]
-//
-// export const cogSponsors2015 = [
-//     {
-//          name: 'Some Company',
-//          contribution: 'Donated x amount of money and more',
-//          image: require('./static/western-union.png')
-//      }
-// ]
-//
-// export const cogSponsors2014 = [
-//     {
-//          name: 'Some Organisation',
-//          contribution: 'Donated x amount of money and more',
-//          image: 'src'
-//      }
-// ]
-//
-// export const cogSponsors2013 = [
-//     {
-//          name: 'Some Organisation 2013',
-//          contribution: 'Donated x amount of money and more',
-//          image: 'src'
-//      }
-// ]
