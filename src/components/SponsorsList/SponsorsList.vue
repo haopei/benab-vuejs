f861b3092f697002b00519eb7ceacb8d0a10e29f
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 14:16:40 -0400

    Moved SponsorsList.vue in its own component directory

---
+++ src/components/SponsorsList/SponsorsList.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+    <section class="section is-medium">
+      <div class="container">
+          <h2 class="subtitle is-2">Sponsors</h2>
+          <div class="sponsor-list-by-year">
+              <h4 class="subtitle is-4">2017</h4>
+              <div class="tile is-ancestor sponsor-list">
+              </div>
+          </div>
+          <div class="sponsor-list-by-year">
+              <h4 class="subtitle is-4">2016</h4>
+              <div class="tile is-ancestor sponsor-list">
+
+              </div>
+          </div>
+          <div class="sponsor-list-by-year">
+              <h4 class="subtitle is-4">2015</h4>
+              <div class="tile is-ancestor sponsor-list">
+
+              </div>
+          </div>
+          <div class="sponsor-list-by-year">
+              <h4 class="subtitle is-4">2014</h4>
+              <div class="tile is-ancestor sponsor-list">
+
+              </div>
+          </div>
+      </div>
+    </section>
+</template>
+
+<script>
+import { cogSponsors2014, cogSponsors2015, cogSponsors2016  } from './sponsors.data.js'
+
+export default {
+    data() {
+        return {
+            testimonials: []
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/SponsorsList/sponsors.data.js
@@ -0,0 +1,34 @@
+// export const allSponsors = [
+//     {
+//          name: 'Grace Kennedy Remittance Services',
+//          contribution: 'Provided branded uniform to volunteers during the exercise.'
+//      },
+//     {
+//          name: 'Version75',
+//          contribution: ''
+//      }
+// ]
+
+export const cogSponsors2016 = [
+    {
+         name: 'Church\'s Chicken',
+         contribution: 'Provided lunch for (n) volunteer tutors every day for (n) days.',
+         image: require('./static/churchs.png')
+     }
+]
+
+export const cogSponsors2015 = [
+    {
+         name: 'Some Company',
+         contribution: 'Donated x amount of money and more',
+         image: require('./static/western-union.png')
+     }
+]
+
+export const cogSponsors2014 = [
+    {
+         name: 'Some Organisation',
+         contribution: 'Donated x amount of money and more',
+         image: 'src'
+     }
+]
