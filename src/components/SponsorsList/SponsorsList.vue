10ba98cb7317e2484a861ccf55aa6f2df458d09f
Haopei Yang <haopeiyang@gmail.com>
2017-03-29 14:49:35 -0400

    Structured SponsorsList.vue component to show sponsor per year

--- src/components/SponsorsList/SponsorsList.vue
+++ src/components/SponsorsList/SponsorsList.vue
@@ -4,39 +4,46 @@
           <h2 class="subtitle is-2">Sponsors</h2>
           <div class="sponsor-list-by-year">
               <h4 class="subtitle is-4">2017</h4>
-              <div class="tile is-ancestor sponsor-list">
-              </div>
+              {{cogSponsors2017}}
           </div>
           <div class="sponsor-list-by-year">
               <h4 class="subtitle is-4">2016</h4>
-              <div class="tile is-ancestor sponsor-list">
-
-              </div>
+              {{cogSponsors2016}}
           </div>
           <div class="sponsor-list-by-year">
               <h4 class="subtitle is-4">2015</h4>
-              <div class="tile is-ancestor sponsor-list">
-
-              </div>
+              {{cogSponsors2015}}
           </div>
           <div class="sponsor-list-by-year">
               <h4 class="subtitle is-4">2014</h4>
-              <div class="tile is-ancestor sponsor-list">
-
-              </div>
+              {{cogSponsors2014}}
+          </div>
+          <div class="sponsor-list-by-year">
+              <h4 class="subtitle is-4">2013</h4>
+              {{cogSponsors2013}}
           </div>
       </div>
     </section>
 </template>
 
 <script>
-import { cogSponsors2014, cogSponsors2015, cogSponsors2016  } from './sponsors.data.js'
-
+import { allCogSponsors  } from './sponsors.data.js'
 export default {
     data() {
         return {
-            testimonials: []
+            cogSponsors2013: [],
+            cogSponsors2014: [],
+            cogSponsors2015: [],
+            cogSponsors2016: [],
+            cogSponsors2017: []
         }
+    },
+    mounted: function() {
+        this.cogSponsors2013 = allCogSponsors.year2013;
+        this.cogSponsors2014 = allCogSponsors.year2014;
+        this.cogSponsors2015 = allCogSponsors.year2015;
+        this.cogSponsors2016 = allCogSponsors.year2016;
+        this.cogSponsors2017 = allCogSponsors.year2017;
     }
 }
 </script>
