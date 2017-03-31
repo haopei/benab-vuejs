489d4f5fd12cc17bdb8318005541d902e10d361c
Haopei Yang <haopeiyang@gmail.com>
2017-03-30 22:58:58 -0400

    Set up simple footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,19 @@
 <template lang="html">
-  <footer>
-      <router-link :to="{ name: 'SponsorPage' }">Sponsors</router-link> |
-      <router-link :to="{ name: 'VolunteerPage' }">Volunteer</router-link> |
-      <router-link :to="{ name: 'MembershipPage' }">Membership</router-link>
-  </footer>
+
+    <footer class="footer">
+    <div class="container">
+    <div class="content has-text-centered">
+      <p>
+          The Benab Foundation
+      </p>
+      <p>
+        <a class="icon" href="https://www.facebook.com/TheBenabFoundation/">
+          <i class="fa fa-facebook"></i>
+        </a>
+      </p>
+    </div>
+    </div>
+    </footer>
 </template>
 
 <script>
