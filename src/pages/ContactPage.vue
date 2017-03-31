19dcc3600771ba862fd2224b2121aca2b70cc8b2
Haopei Yang <haopeiyang@gmail.com>
2017-03-30 22:58:50 -0400

    Set up simple contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,10 +1,20 @@
 <template lang="html">
   <div class="page--contact">
 
+    <section class="hero is-info is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Say Hello
+          </h1>
+        </div>
+      </div>
+    </section>
+
     <section class="section">
       <div class="container">
         <div class="heading">
-          <h2 class="title is-h2">Say Hello</h2>
+          <h2 class="title is-h2"></h2>
         </div>
         <h6 class="subtitle is-6">We are always happy to connect with thinkers and doers.</h6>
 
