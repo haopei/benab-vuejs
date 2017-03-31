baf4bc3a966c68df17ce9a9cf5e6cd67330f5d21
Haopei Yang <haopeiyang@gmail.com>
2017-03-31 11:37:07 -0400

    Move all GetInvolved-type pages to pages/GetInvolved/**

---
+++ src/pages/GetInvolved/GetInvolvedVolunteerPage.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+    <div class="page page--volunteer">
+
+        <section class="hero is-medium is-primary background--cog-hero">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title is-spaced">
+                  Volunteer
+              </h1>
+              <h4 class="subtitle is-4">
+                  <small>
+                      A rewarding experience.
+                  </small>
+              </h4>
+            </div>
+          </div>
+        </section>
+
+        <div class="requirements">
+            <h3>Requirements</h3>
+            <ul>
+                <li>Hero</li>
+                <li>Encourage people to volunteer with good reason</li>
+                <li>Vounteer experience</li>
+                <li>Existing volunteers</li>
+                <li>list of Roles</li>
+                <li>Link to form</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
