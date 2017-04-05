9cd5f4d7cf17dc98496cbd25cd22b5ae91f8780c
Haopei Yang <haopeiyang@gmail.com>
2017-04-05 18:57:47 -0400

    Add Media Coverage section on HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,8 +7,45 @@
       </div>
     </section>
     <cog-partial></cog-partial>
-    <get-involved></get-involved>
+    <section class="section media-coverage has-text-centered">
+        <div class="container">
+          <div class="heading">
+            <h2 class="subtitle is-2">In The Media</h2>
+          </div>
+
+          <br><br>
+
+          <div class="columns">
+            <div class="column">
+              <a href="http://demerarawaves.com/2015/07/07/benab-foundation-helping-to-improve-numeracy-literacy-in-several-city-schools/">
+                <img src="./static/demerara-waves-logo.png" class="media-coverage__logo" alt="">
+                <h4 class="subtitle is-4"><em>"Benab Foundation helping to improve numeracy, literacy in several City schools"</em></h4>
+              </a>
+            </div>
+
+            <div class="column">
+              <a href="https://guyanachronicle.com/2016/06/18/benab-foundation-rolls-out-literacy-prog-in-july">
+                <img src="./static/chronicle-logo.png" class="media-coverage__logo" alt="">
+                <h4 class="subtitle is-4">
+                  <em>"Benab Foundation rolls out literacy programme in July"</em>
+                </h4>
+              </a>
+            </div>
+
+            <div class="column">
+              <a href="http://guyanatimesgy.com/literacy-numeracy-classes-for-100-students-benab-foundation/">
+                <img src="./static/guyana-times-logo.png" class="media-coverage__logo" alt="">
+                <h4 class="subtitle is-4">
+                  <em>"Literacy, numeracy classes for 100 students – Benab Foundation"</em>
+                </h4>
+              </a>
+            </div>
+          </div>
+        </div>
+    </section>
     <sponsors-list></sponsors-list>
+    <get-involved></get-involved>
+
   </div>
 </template>
 
@@ -31,5 +68,13 @@ export default {
 </script>
 
 <style lang="scss">
+
+  .media-coverage {
+    background: #f5f5f5;
+
+    &__logo {
+      height: 60px;
+    }
+  }
 
 </style>
