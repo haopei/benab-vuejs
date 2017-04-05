bba921ccbda3b4c972e5c9a7dee61b30ee511919
Haopei Yang <haopeiyang@gmail.com>
2017-04-05 15:02:29 -0400

    Add photos to get-involved-choose

--- src/pages/GetInvolved/GetInvolvedChoosePage.vue
+++ src/pages/GetInvolved/GetInvolvedChoosePage.vue
@@ -1,14 +1,14 @@
 <template lang="html">
 
     <div class="get-involved-choose">
-        <section class="hero is-medium is-primary is-bold">
+        <section class="hero hero--get-involved-choose is-large">
           <div class="hero-body">
             <div class="container">
-              <h1 class="title">
-                Get Involved
+              <h1 class="title is-1 hero--get-involved-choose__title">
+                <!-- Get Involved -->
               </h1>
-              <h2 class="subtitle">
-                  Some inspirational sentence, or something.
+              <h2 class="subtitle is-2 hero--get-involved-choose__subtitle">
+                  <!-- Some inspirational sentence, or something. -->
               </h2>
             </div>
           </div>
@@ -16,36 +16,47 @@
 
         <section class="section">
             <div class="container">
+                <h1 class="title is-1">Get involved</h1>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+
                 <div class="columns">
                     <div class="column">
-                        <h4 class="subtitle is-4">Volunteer</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                        <br>
-                        <router-link :to="{name: 'GetInvolvedVolunteerPage'}" class="button is-primary">Volunteer Now</router-link>
+                        <div class="box">
+                            <h4 class="title is-spaced is-4">Volunteer</h4>
+                            <img src="./static/2.jpg" alt="" class="is-fullwidth">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <br>
+                            <router-link :to="{name: 'GetInvolvedVolunteerPage'}" class="button is-primary">Volunteer Now</router-link>
+                        </div>
                     </div>
+
                     <div class="column">
-                        <h4 class="subtitle is-4">Donate</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                        <br>
-                        <router-link :to="{name: 'GetInvolvedSponsorsPage'}" class="button is-primary">Donate Now</router-link>
+                        <div class="box">
+                            <h4 class="title is-spaced is-4">Donate</h4>
+                            <img src="./static/3.jpg" alt="" class="is-fullwidth">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <br>
+                            <router-link :to="{name: 'GetInvolvedSponsorsPage'}" class="button is-primary">Donate Now</router-link>
+                        </div>
                     </div>
+
                     <div class="column">
-                        <h4 class="subtitle is-4">Join Our Team</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                        <br>
-                        <router-link :to="{name: 'GetInvolvedMembershipPage'}" class="button is-primary">Join Now</router-link>
+                        <div class="box">
+                            <h4 class="title is-spaced is-4">Join Our Team</h4>
+                            <img src="./static/3.jpg" alt="" class="is-fullwidth">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <br>
+                            <router-link :to="{name: 'GetInvolvedMembershipPage'}" class="button is-primary">Join Now</router-link>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </section>
-
-        <div class="requirements">
-            <ul>
-                <li>Hero</li>
-                <li>Short paragraph to entice involvement</li>
-                <li>Link three ways to get involved: volunteering, sponsorship, membership</li>
-            </ul>
-        </div>
     </div>
 
 </template>
@@ -55,5 +66,16 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+
+.hero--get-involved-choose {
+    background: url('./static/1.jpg') center/cover;
+
+    &__title {
+        color: #fff;
+    }
+    &__subtitle {
+        color: #fff;
+    }
+}
 </style>
