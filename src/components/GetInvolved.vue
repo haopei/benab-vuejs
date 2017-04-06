89309bf9627c171f55b2a0b9b7015e947ca14986
Haopei Yang <haopeiyang@gmail.com>
2017-04-05 22:58:02 -0400

    Change size of heading in GetInvolved partial

--- src/components/GetInvolved.vue
+++ src/components/GetInvolved.vue
@@ -2,26 +2,25 @@
     <section class="section partial--get-involved">
       <div class="container">
         <div class="heading"><h2 class="title is-2">Get Involved</h2></div>
-        <h6 class="subtitle is-h6">Be part of the impact</h6>
-        [use tabs for this]
+        <p class="subtitle">Be part of the impact</p>
         <hr>
         <div class="columns">
             <div class="column">
-                <h4 class="subtitle is-4">Become a Volunteer</h4>
+                <h5 class="subtitle is-5">Become a Volunteer</h5>
                 <img :src="require('./../assets/collage.jpg')" alt="">
                 <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
                 <router-link :to="{name: 'GetInvolvedVolunteerPage'}" class="button is-primary is-outlined is-inverted">See Where You Fit</router-link>
             </div>
             <div class="column">
-                <h4 class="subtitle is-4">Become a Sponsor</h4>
+                <h5 class="subtitle is-5">Become a Sponsor</h5>
                 <img :src="require('./../assets/collage.jpg')" alt="">
                 <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
                 <router-link :to="{name: 'GetInvolvedSponsorsPage'}" class="button is-primary is-outlined is-inverted">Fuel Our Fire</router-link>
             </div>
             <div class="column">
-                <h4 class="subtitle is-4">Join Our Team</h4>
+                <h5 class="subtitle is-5">Join Our Team</h5>
                 <img :src="require('./../assets/collage.jpg')" alt="">
                 <!-- <p class="small">It will be the one of the most rewarding experiences in your life</p> -->
                 <br>
