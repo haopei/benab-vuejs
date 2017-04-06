70a87a832bc2425d49bf3f15659134690899f257
Haopei Yang <haopeiyang@gmail.com>
2017-04-05 21:49:45 -0400

    Themed Cog2017Page features tab

--- src/pages/Cog2017Page/CogFeaturesTabs.vue
+++ src/pages/Cog2017Page/CogFeaturesTabs.vue
@@ -10,22 +10,29 @@
             </div>
             <div class="tabs__content has-text-centered">
                 <div v-if="tabs.active == 'tailored'" class="">
-                    <img :src="require('./static/tabs/tailor.jpg')" alt="">
-                    <br><br>
-                    <h4 class="subtitle is-4">Lessons Across All Levels</h4>
-                    <p>Our students are assessed and placed into specific classrooms <br> where lessons are designed to patch and strengthen their <br> understanding of core concepts.</p>
+                    <div class="content">
+                        <img :src="require('./static/tabs/tailor.jpg')" alt="">
+                        <br><br>
+                        <h4 class="subtitle is-4">Lessons Across All Levels</h4>
+                        <p>Our students are assessed and placed into specific classrooms <br> where lessons are designed to patch and strengthen their <br> understanding of core concepts.</p>
+                    </div>
+
                 </div>
                 <div v-if="tabs.active == 'volunteers'" class="">
-                    <img :src="require('./static/tabs/volunteers.jpg')" alt="">
-                    <br><br>
-                    <h4 class="subtitle is-4">Crème de la crème Volunteers</h4>
-                    <p>Our students are assessed and placed into specific classrooms <br> where lessons are designed to patch and strengthen their <br> understanding of core concepts.</p>
+                    <div class="content">
+                        <img :src="require('./static/tabs/volunteers.jpg')" alt="">
+                        <br><br>
+                        <h4 class="subtitle is-4">Crème de la crème Volunteers</h4>
+                        <p>Our students are assessed and placed into specific classrooms <br> where lessons are designed to patch and strengthen their <br> understanding of core concepts.</p>
+                    </div>
                 </div>
                 <div v-if="tabs.active == 'focused-mentorship'" class="">
-                    <img :src="require('./static/tabs/one-on-one.jpg')" alt="">
-                    <br><br>
-                    <h4 class="subtitle is-4">1-on-1 Attention on Demand</h4>
-                    <p>Our volunteers are deployed to the classroom in teams <br> and continuously assess our students understanding. <br> This means there is more attention per student. </p>
+                    <div class="content">
+                        <img :src="require('./static/tabs/one-on-one.jpg')" alt="">
+                        <br><br>
+                        <h4 class="subtitle is-4">1-on-1 Attention on Demand</h4>
+                        <p>Our volunteers are deployed to the classroom in teams <br> and continuously assess our students understanding. <br> This means there is more attention per student. </p>
+                    </div>
                 </div>
             </div>
 
@@ -58,5 +65,12 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+    .tabs__content {
+        background: #ffffff;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to top, #f5f5f5, #ffffff);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to top, #f5f5f5, #ffffff); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        padding: 3rem 1.5rem;
+    }
+
 </style>
