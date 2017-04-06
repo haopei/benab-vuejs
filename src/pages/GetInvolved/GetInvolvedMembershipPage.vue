dc01b98d4085d316b0c8cca0db3de786dd27d906
Haopei Yang <haopeiyang@gmail.com>
2017-04-06 11:36:21 -0400

    Create membership page

--- src/pages/GetInvolved/GetInvolvedMembershipPage.vue
+++ src/pages/GetInvolved/GetInvolvedMembershipPage.vue
@@ -1,7 +1,33 @@
 <template lang="html">
     <div class="page page--members">
-        <h3>Members</h3>
+        <section class="hero is-primary is-bold is-medium">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">Join The Team</h1>
+                    <p class="subtitle">Continuously challenge yourself in creative projects which <br> aim to improve the education levels in Guyana</p>
+
+                </div>
+            </div>
+        </section>
+        <div class="section">
+            <div class="columns">
+                <div class="column">
+                    <div class="has-text-centered">
+                        <h4 class="title is-4">Open Specialized Roles</h4>
+                        <ul>
+                            <li>Photographer</li>
+                            <li>Operations Officer</li>
+                            <li>Marketing Officer</li>
+                        </ul>
+                        <br>
+                        <a href="#" class="button is-primary">Join Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 </template>
 
 <script>
@@ -9,5 +35,8 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    .hero--get-involved-membership {
+        background: url('./static/1.jpg') center/cover;
+    }
 </style>
