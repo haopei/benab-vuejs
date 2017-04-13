8905462adb1d34fa1103718cfe1b332e32387d1c
Haopei Yang <haopeiyang@gmail.com>
2017-04-13 11:18:24 -0400

    Finish testimonial component

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,27 +1,51 @@
 <template lang="html">
+
+  <div class="testimonial-container">
     <article class="media testimonial">
-    <figure class="media-left">
-      <p class="image is-64x64">
-        <img src="http://bulma.io/images/placeholders/128x128.png">
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-          "<em>{{ testimonial.quote }}</em>""
-          <br>
-          <strong>{{ testimonial.name }}</strong>
-          <br>
-          <small>{{ testimonial.role }}</small>
+      <div class="columns">
+          <div class="column is-one-quarter">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img :src="testimonial.image">
+              </p>
+            </figure>
+          </div>
+          <div class="column">
+            <div class="media-content">
+              <div class="content">
+                  "<em>{{ testimonial.quote }}</em>"
+                  <br>
+                  <strong>{{ testimonial.name }}</strong>
+                  <br>
+                  <small>{{ testimonial.role }}</small>
+              </div>
+            </div>
+          </div>
       </div>
-    </div>
   </article>
+  </div>
+
 </template>
 
 <script>
+
 export default {
   props: ['testimonial']
 }
+
+
+
 </script>
 
-<style lang="css">
+<style lang="scss">
+
+.testimonial {
+  background: rgba(255, 255, 255, 0.2);
+  display: inline-block;
+  padding: 1rem;
+  margin: 1rem 0;
+  max-width: 450px;
+  border-radius: 5px;
+}
+
 </style>
