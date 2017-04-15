10cbd0b1c55fe67668fb6d6e1b83e77b3c2498f7
Haopei Yang <haopeiyang@gmail.com>
2017-04-14 21:55:22 -0400

    Create VolunteersList to be used inside projects

---
+++ src/components/VolunteersList/VolunteerItem.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="volunteer">
+    <article class="media">
+      <figure class="media-left">
+        <p class="image is-48x48">
+          <img :src="image">
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{name}}</strong> <br>
+            <small>{{role}}</small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <!-- <i class="fa fa-fw fa-star" aria-hidden="true"></i> -->
+      </div>
+    </article>
+  </div>
+
+
+
+  <!-- <div class="volunteer-container">
+    <div class="volunteer">
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-48x48 media-right is-pulled-right">
+            <p class="image is-48x48">
+              <img :src="image" class="volunteer__author-thumbnail">
+            </p>
+          </figure>
+        </div>
+        <div class="column">
+            <div class="">
+              <strong>{{name}}</strong>
+            </div>
+            <small>{{role}}</small>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+</template>
+
+<script>
+export default {
+  props: ['name', 'image', 'role']
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/VolunteersList/VolunteersList.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="volunteers-list">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4 col-xs-6 col-md-3 col-lg-2" v-for="vol in volunteers">
+          <volunteer
+            :name="vol.name"
+            :image="vol.image"
+            :role="vol.role"
+            :key="vol.name"
+            class="volunteer-item"
+          />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Volunteer from './VolunteerItem.vue';
+export default {
+  props: {
+    volunteers: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Volunteer
+  }
+}
+</script>
+
+<style lang="scss">
+
+.volunteer-item {
+  margin-bottom: 14px
+}
+
+</style>
