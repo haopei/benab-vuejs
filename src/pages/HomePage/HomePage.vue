c8e57029611b04c62fa107d101823613c0c2f10a
Haopei Yang <haopeiyang@gmail.com>
2017-04-22 13:41:11 -0400

    Update front page

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -2,21 +2,38 @@
   <div class="page page--home">
 
 
-    <section class="section has-text-centered is-fullheight intro">
+    <!-- <section class="section has-text-centered is-fullheight intro">
       <div class="container">
         <img src="./../../assets/benab-logo.png" alt="">
         <br> <br>
-        <h1 class="title is-1 is-spaced">Moving Education Forward</h1>
-        <!-- <testimonial :image="require('./static/mkb.jpg')">
+        <h1 class="title is-1 is-handwriting is-spaced">Moving Education Forward</h1>
+        <testimonial :image="require('./static/mkb.jpg')">
             <span slot="authorName">Jane Doe</span>
             <span slot="authorRole">Volunteer</span>
             <span slot="authorQuote">We are a small group of folks who identify education-related problems and solving them.</span>
-        </testimonial> -->
+        </testimonial>
         <router-link :to="{ name: 'AboutPage' }"><a class="button is-primary is-large">More About Us</a></router-link>
+      </div>
+    </section> -->
+
+    <section class="hero intro is-large">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <div class="has-text-centered">
+                <img src="./../../assets/benab-logo.png" alt="">
+                <p class="subtitle is-3 is-spaced">We execute</p>
+                <h1 class="title is-1 is-handwriting is-spaced">socially-impactful ideas</h1>
+                <p class="subtitle is-3 is-spaced">through volunteerism</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 
-    <section class="section is-medium has-text-centered">
+    <!-- <section class="section is-medium has-text-centered">
       <div class="container">
         <div class="columns">
           <div class="column">
@@ -45,7 +62,7 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
     <cog-partial></cog-partial>
 
@@ -91,7 +108,7 @@
 <script>
 
 // Component Imports
-import GetInvolved from './../../components/GetInvolved.vue'
+import GetInvolved from './../../components/GetInvolved/GetInvolved.vue'
 import SponsorsList from './../../components/SponsorsList/SponsorsList.vue'
 import Testimonial from './../../components/Testimonial.vue';
 
@@ -112,8 +129,8 @@ export default {
 
   .intro {
     background: #cfcfaa;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to bottom, #f5f5f5, #b7b77d);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to bottom, #f5f5f5, #b7b77d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: -webkit-linear-gradient(to bottom, #f5f5f5, #cccc90);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to bottom, #f5f5f5, #cccc90); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   }
 
   .media-coverage {
